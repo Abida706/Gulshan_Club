24c0c01818795c16822bc56fb755476c191f5523
Nihal007-m <2230904@iub.edu.bd>
2025-08-03 01:53:09 +0600

    Nihal

---
+++ src/main/java/com/example/gulshan_club/ClubMember/VotingPanelController.java
@@ -0,0 +1,38 @@
+package com.example.gulshan_club.ClubMember;
+
+import javafx.event.ActionEvent;
+import javafx.scene.layout.VBox;
+
+public class VotingPanelController
+{
+    @javafx.fxml.FXML
+    private VBox optionContainerVBox;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void handleBackButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handeCRadioButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleBRadioButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleARadioButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleLogoutButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleVoteButton(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/example/gulshan_club/ClubPresident/MeetingSchedulerController.java
@@ -0,0 +1,36 @@
+package com.example.gulshan_club.ClubPresident;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class MeetingSchedulerController
+{
+    @javafx.fxml.FXML
+    private TextField titleTextField;
+    @javafx.fxml.FXML
+    private AnchorPane dateDatePicker;
+    @javafx.fxml.FXML
+    private ComboBox timeComboBox;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void handleBackButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleCancelButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleLogoutButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleScheduleButton(ActionEvent actionEvent) {
+    }
+}
